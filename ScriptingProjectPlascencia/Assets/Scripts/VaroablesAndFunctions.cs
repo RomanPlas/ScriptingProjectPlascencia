1940f83a6e405ca9ba175aa77f5f69f039815911
Roman Plascencia <705392@student.dusd.net>
2022-10-27 09:04:02 -0700

    Added project
    Added the scripting project to the repo

---
+++ ScriptingProjectPlascencia/Assets/Scripts/VaroablesAndFunctions.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaroablesAndFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+        int myInt = 5;
+
+
+
+        void Start()
+        {
+            myInt = MultiplyByTwo(myInt); ;
+            Debug.Log(myInt);
+        }
+
+        int MultiplyByTwo(int number)
+        {
+            int result;
+            result = number * 2;
+            return result;
+        }
+
+        
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
