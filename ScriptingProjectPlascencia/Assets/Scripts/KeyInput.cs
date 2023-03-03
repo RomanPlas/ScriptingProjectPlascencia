13bbd309742091498046ce3fa1f9e97e14b3f6f5
Roman Plascencia <705392@student.dusd.net>
2023-03-03 09:54:19 -0800

    added getkey
    added new scrpit

---
+++ ScriptingProjectPlascencia/Assets/Scripts/KeyInput.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInput : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool down = Input.GetKeyDown(KeyCode.Space);
+        bool held = Input.GetKey(KeyCode.Space);
+        bool up = Input.GetKeyUp(KeyCode.Space);
+
+        if(down)
+        {
+            Debug.Log("Space is down: " + down);
+        }
+
+        else if(held)
+        {
+            Debug.Log("Space is held: " + held);
+        }
+        else if(up)
+        {
+            Debug.Log("Space is Up" + up);
+        }
+        Debug.Log("Space is down: " + down);
+        Debug.Log("Space is held: " + held);
+        Debug.Log("Space is up: " + up);
+    }
+}
